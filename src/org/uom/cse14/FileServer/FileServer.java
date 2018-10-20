e6ac83cdeb29da13f960802859a42ad12b327221
Pasindu <pasinduath@gmail.com>
2018-10-20 20:19:01 +0530

    RMI Completed

--- src/org/uom/cse14/FileServer/FileServer.java
+++ src/org/uom/cse14/FileServer/FileServer.java
@@ -23,7 +23,7 @@ public class FileServer implements FileInterface {
         }*/
 
     try{
-
+        System.setProperty("java.rmi.server.hostname","192.168.8.102");
         FileServer obj = new FileServer();
         FileInterface stub = (FileInterface)UnicastRemoteObject.exportObject(obj, 0);
         Registry registry = LocateRegistry.createRegistry(3045);
@@ -38,12 +38,13 @@ public class FileServer implements FileInterface {
 
     public byte[] downloadFile(String fileName){
         try {
-            File file = new File(fileName);
+            File file = new File(FileServer.class.getProtectionDomain().getCodeSource().getLocation().getPath()+"/org/uom/cse14/FileServer/"+fileName);
             byte buffer[] = new byte[(int)file.length()];
             BufferedInputStream input = new
-                    BufferedInputStream(new FileInputStream(fileName));
+                    BufferedInputStream(new FileInputStream(FileServer.class.getProtectionDomain().getCodeSource().getLocation().getPath()+"/org/uom/cse14/FileServer/"+fileName));
             input.read(buffer,0,buffer.length);
             input.close();
+            System.out.printf(Integer.toString(buffer.length));
             return(buffer);
         } catch(Exception e){
             System.out.println("FileImpl: "+e.getMessage());
