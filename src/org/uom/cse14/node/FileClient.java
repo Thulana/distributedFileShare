f467ce0de94b10fcce526e2d48a296fa788af35f
Pasindu Senanayake <pasinduath@gmail.com>
2018-10-20 20:20:53 +0530

    RMI Completed II

--- src/org/uom/cse14/node/FileClient.java
+++ src/org/uom/cse14/node/FileClient.java
@@ -3,42 +3,29 @@ package org.uom.cse14.node;
 import org.uom.cse14.FileServer.FileInterface;
 
 import java.io.*;
-import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class FileClient{
     public static void main(String argv[]) {
-   /*     if(argv.length != 2) {
-            System.out.println("Usage: java FileClient fileName servermachineName");
-            System.exit(0);
-        }
+
+        System.setProperty("java.rmi.server.hostname","192.168.8.102");
         try {
-            String name = "//" + argv[1] + "/FileServer";
-            FileInterface fi = (FileInterface) Naming.lookup(name);
-            byte[] filedata = fi.downloadFile(argv[0]);
-            File file = new File(argv[0]);
+            Registry registry = LocateRegistry.getRegistry("192.168.8.102",3045);
+            FileInterface stub = (FileInterface) registry.lookup("Hello");
+            byte[] fileData = stub.downloadFile("TestFile.txt");
+            System.out.printf(Integer.toString(fileData.length));
+            File file = new File("TestFile.txt");
             BufferedOutputStream output = new
-                    BufferedOutputStream(new FileOutputStream(file.getName()));
-            output.write(filedata,0,filedata.length);
+                    BufferedOutputStream(new FileOutputStream(FileClient.class.getProtectionDomain().getCodeSource().getLocation().getPath()+"/org/uom/cse14/FileServer/"+file.getName()));
+            output.write(fileData,0,fileData.length);
             output.flush();
             output.close();
+            String response = Integer.toString(stub.addTwo(5,10));
+            System.out.println("response: " + response);
         } catch(Exception e) {
             System.err.println("FileServer exception: "+ e.getMessage());
             e.printStackTrace();
-        }*/
-
-        try {
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1",3045);
-            FileInterface stub = (FileInterface) registry.lookup("Hello");
-            String response = Integer.toString(stub.addTwo(5,10));
-            System.out.println("response: " + response);
-
-        } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
         }
-
-
     }
 }
